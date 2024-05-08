227c352fb6ccd1426a0d65aa7e186c645aed8c20
GicheruBenjamin <projectbuild201@gmail.com>
2024-05-08 17:58:30 +0300

    seq1

---
+++ TypesPy/seq.py
@@ -0,0 +1,10 @@
+#importing sequence
+from typing import Sequence
+
+#List, tuples and strings are called sequences because they can be indexed. 
+name: Sequence = 'I love milk..'
+my_hobbies: Sequence = ['Coding', 'Sleeping', 'Eating']
+my_bmi: Sequence = (5.7,56)
+
+#no errors 
+
