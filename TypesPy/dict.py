095c30d06d79fe987fe4eb85050a8290251488ae
GicheruBenjamin <projectbuild201@gmail.com>
2024-08-13 16:54:18 +0300

    C4.54

---
+++ TypesPy/dict.py
@@ -0,0 +1,18 @@
+
+
+from typing import dict , Sequence,list
+
+
+my_hobbies: dict[str, Sequence] = {
+    "name": "Alice",
+    "hobbies": ["Coding", "Sleeping", "Eating"],
+}
+
+print(my_hobbies["hobbies"])
+
+people:dict[str,List[str]] = {
+    "friends": ["john", "jane"],
+    "family": ["dad", "mom"]
+}
+
+print(people["friends"[1]])
