95220e75469f7717323a949abb73951785103906
GicheruBenjamin <projectbuild201@gmail.com>
2024-08-21 16:41:35 +0300

    C4.41

---
+++ TypesPy/list.py
@@ -0,0 +1,7 @@
+
+from typing import List
+
+my_hobbies: List[str] = ["Coding", "Sleeping", "Eating"]
+Companies: List[str] = ["Microsoft", "Google", "Apple", "Facebook"]
+
+print(f"I would love working in {Companies[1]}")
