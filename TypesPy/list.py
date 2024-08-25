541164bc8d014309a96a2f0776a74452b2331878
GicheruBenjamin <projectbuild201@gmail.com>
2024-08-25 17:47:13 +0300

    5.47

--- TypesPy/list.py
+++ TypesPy/list.py
@@ -3,5 +3,6 @@ from typing import List
 
 my_hobbies: List[str] = ["Coding", "Sleeping", "Eating"]
 Companies: List[str] = ["Microsoft", "Google", "Apple", "Facebook"]
+cars: List[dict[str: str]] = [    {"make": "Toyota", "model": "Corolla", "year": "2015"},    {"make": "Honda", "model": "Civic", "year": "2018"},    {"make": "Ford", "model": "Mustang", "year": "2019"},    {"make": "Chevrolet", "model": "Camaro", "year": "2020"}]
 
 print(f"I would love working in {Companies[1]}")
