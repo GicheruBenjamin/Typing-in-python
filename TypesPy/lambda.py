76f130a02fc94949861d1ece11fd8522c0348cb8
GicheruBenjamin <benjaminnjuguna77@gmail.com>
2024-09-28 21:34:58 +0300

    C9.34

--- TypesPy/lambda.py
+++ TypesPy/lambda.py
@@ -10,3 +10,15 @@ great_food: callable = lambda food: print(f"I like {food}.")
 great_food("pizza") 
 
 
+
+
+ops : dict[any , callable]= {
+    '+': lambda x, y: x + y,
+    '-': lambda x, y: x - y,
+    '*': lambda x, y: x * y,
+    '/': lambda x, y: x / y if y != 0 else "Error: Division by zero",    
+}
+
+
+
+print(ops['+'](3, 2))
